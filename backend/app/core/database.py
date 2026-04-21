f26d8583b599d56de7becde29d0974383572dc2a
Christyan Simbolon <sambungdata@gmail.com>
2026-04-21 22:43:54 +0700

    feat-backend: add core database

---
+++ backend/app/core/database.py
@@ -0,0 +1,15 @@
+"""
+app/core/database.py
+"""
+from app.core.config import settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=10,
+    max_overflow=20,
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
